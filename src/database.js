b328589cdecfa28b7dabf58182c92f6d4d1828d0
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2022-10-09 15:11:40 -0400

    Adicionado variáveis de ambiente

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URI =  'mongodb+srv://andremartins30:Kabana12@cluster0.7qd9cly.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URI =  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.7qd9cly.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(MONGO_URI, {
     useUnifiedTopology: true,
