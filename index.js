be11cd7058a9ad2cbb00a1a81a6a02e1f2f1ec3c
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2022-10-11 21:34:55 -0400

    Alterações para deploy

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const app = require('./src/server');
 require('./src/database');
 
+const port = process.env.PORT || 4000
 
-app.listen(app.get('port'), () => {
-    console.log('Conectado no http://localhost:'+app.get('port'))
+app.listen(port, () => {
+    console.log(`Conectado no http://localhost:${port}`)
 })
