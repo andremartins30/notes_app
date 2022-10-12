cb0e9b101b27748d286cd19ae0c1a95eceb61aaa
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2022-10-12 18:46:17 -0400

    Atualização do mongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URI =  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.7qd9cly.mongodb.net/?retryWrites=true&w=majority`
+const MONGO_URI =  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.7qd9cly.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
 
 mongoose.connect(MONGO_URI, {
     useUnifiedTopology: true,
